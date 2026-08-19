786d8bed6dee6edd51945a9bd4460b4380fef5f1
Ammar Qureshi <i.asqtofficial@gmail.com>
2026-08-19 14:39:02 +0500

    name changes

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import json
 import time
 import concurrent.futures
 
-st.set_page_config(page_title="CardLoom DRV", page_icon="📚", layout="centered")
+st.set_page_config(page_title="CardLoom", page_icon="📚", layout="centered")
 
 st.title("📚 CardLoom")
 st.caption("Upload lecture notes or a textbook PDF, get flashcards and a quiz to study from.")
